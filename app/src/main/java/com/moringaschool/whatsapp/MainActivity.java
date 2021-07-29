7d70480156cbf0c6001b61da07b6e85a3da6aa05
Abdallah@moringa <abdallah.abdul@student.moringaschool.com>
2021-07-29 11:48:23 +0300

    add the changes made

--- app/src/main/java/com/moringaschool/whatsapp/MainActivity.java
+++ app/src/main/java/com/moringaschool/whatsapp/MainActivity.java
@@ -1,16 +1,14 @@
 package com.moringaschool.whatsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.support.v4.
-
 
 import android.os.Bundle;
 import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
-private Toolbar mToolbar;
-    private Object Toolbar;
+       private Toolbar mToolbar;
+
 
 
     @Override
@@ -18,12 +16,11 @@ private Toolbar mToolbar;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mToolbar=(Toolbar) findViewById(R.id.main_page_toolbar);
-    setSupportActionBar(mToolbar);
-    getSupportActionBar().setTitle("WhatsApp");
+        mToolbar = (Toolbar) findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("WhatsApp");
 
-    }
 
-   private void setSupportActionBar(Toolbar mToolbar) {
+
     }
 }
